e45e45f039fedb1a2a7e0321773b9a9cf7b42779
BlazeSN860 <virajprabhup@gmail.com>
2024-03-16 19:35:17 +0530

    server update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import http from 'http';
-import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
@@ -13,7 +12,8 @@ app.use(cors({
 
 app.use(compression());
 app.use(cookieParser());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const server = http.createServer(app);
 
