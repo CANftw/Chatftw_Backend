eec63fd52a426271633bbf0cd1262b45ee546edf
BlazeSN860 <virajprabhup@gmail.com>
2024-05-09 01:00:36 +0530

    new commmit.

--- src/controllers/room.controller.ts
+++ src/controllers/room.controller.ts
@@ -1,3 +1,4 @@
+import express from 'express';
 import { getRooms, getRoomByName, getRoomById, createRoom } from '../db/rooms'
 import { encryptString, decryptString } from '../utils/crypt';
 export const addMessageToRoom = async (roomName: any, user: any, message: any) => {
@@ -23,5 +24,17 @@ export const addMessageToRoom = async (roomName: any, user: any, message: any) =
     }
     catch (error) {
         console.log(error);
+    }
+}
+
+export const getMessagesFromRoom = async (req: express.Request, res: express.Response) => { 
+    const { room }: { room: string } = req.body;
+
+    try {
+        const messages = await getRoomByName(room);
+        return res.status(200).json(messages);
+    } catch (error) {
+        console.log(error);
+        return res.json({ error: 'DB Error Not Found'}).sendStatus(400);
     }
 }
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import { getAllUsers, deleteUser, updateUser } from '../controllers/user.controller';
+import { getMessagesFromRoom } from '../controllers/room.controller';
 import { isAuthenticated, isOwner, authenticateToken } from '../middleware/index';
 
 export default (router: express.Router) => {
     router.get('/users', authenticateToken, getAllUsers);
     router.delete('/users/:id', authenticateToken, isOwner, deleteUser);
     router.patch('/users/:id', authenticateToken, isOwner, updateUser);
+    router.get('/messages',authenticateToken, getMessagesFromRoom)
 };
